98e9b678b8eec76a1400b34ae787e7701119470e
pchchv <ipchchv@gmail.com>
2023-01-18 13:38:32 +0300

    Add tests for deposit method

--- server_test.go
+++ server_test.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"bytes"
 	"io"
 	"log"
 	"net/http"
@@ -16,9 +17,11 @@ func TestServerPing(t *testing.T) {
 	if err != nil {
 		t.Fatal(err)
 	}
+
 	if res.StatusCode != http.StatusOK {
 		t.Errorf("status not OK")
 	}
+
 	defer func(Body io.ReadCloser) {
 		err := Body.Close()
 		if err != nil {
@@ -28,11 +31,14 @@ func TestServerPing(t *testing.T) {
 	if err != nil {
 		t.Fatal(err)
 	}
+
 	body, err := io.ReadAll(res.Body)
 	if err != nil {
 		t.Fatal(err)
 	}
+
 	b := string(body)
+
 	if !strings.Contains(b, "Balance") {
 		t.Fatal()
 	}
@@ -44,6 +50,61 @@ func TestLoadPing(t *testing.T) {
 	targeter := vegeta.NewStaticTargeter(vegeta.Target{
 		Method: "GET",
 		URL:    "http://127.0.0.1:" + getEnvValue("PORT") + "/ping",
+	})
+
+	attacker := vegeta.NewAttacker()
+	var metrics vegeta.Metrics
+
+	for res := range attacker.Attack(targeter, rate, duration, "Big Bang!") {
+		metrics.Add(res)
+	}
+
+	metrics.Close()
+	log.Printf("99th percentile: %s\n", metrics.Latencies.P99)
+}
+
+func TestDeposit(t *testing.T) {
+	body := []byte(`{
+		"id" : "ec6761fa-4b02-4e93-a213-8fa96eb44d15",
+		"funds" : 33.3
+	}`)
+
+	req, err := http.NewRequest(http.MethodPatch, "http://127.0.0.1:"+getEnvValue("PORT")+"/deposit", bytes.NewBuffer(body))
+	req.Header.Set("Content-Type", "application/json")
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	res, err := http.DefaultClient.Do(req)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	if res.StatusCode != http.StatusOK {
+		t.Errorf("status not OK")
+	}
+
+	defer func(Body io.ReadCloser) {
+		err := Body.Close()
+		if err != nil {
+			t.Error(err)
+		}
+	}(res.Body)
+	if err != nil {
+		t.Fatal(err)
+	}
+}
+
+func TestLoadDeposit(t *testing.T) {
+	rate := vegeta.Rate{Freq: 1000, Per: time.Second}
+	duration := 5 * time.Second
+	targeter := vegeta.NewStaticTargeter(vegeta.Target{
+		Method: "POST",
+		URL:    "http://127.0.0.1:" + getEnvValue("PORT") + "/deposit",
+		Body: []byte(`{
+			"id" : "ec6761fa-4b02-4e93-a213-8fa96eb44d15",
+			"funds" : 33.3
+		}`),
 	})
 	attacker := vegeta.NewAttacker()
 	var metrics vegeta.Metrics
